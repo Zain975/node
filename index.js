078310837ecc4478d7f1bf39363421a300ac20b4
Zain975 <zaingujjer97531@gmail.com>
2022-04-06 07:38:20 +0500

    comit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const videoRoute = require("./routes/video");
 dotenv.config();
 const port = process.env.PORT || 8000;
 mongoose.connect(
-  "mongodb://admin:1133557799@cluster0.59mxf.mongodb.net/FirstDatabase?retryWrites=true&w=majority" ||
+  "mongodb+srv://admin:1133557799@cluster0.59mxf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ||
     process.env.MONGO_URL
 );
 
