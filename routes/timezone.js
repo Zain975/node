a96be67476ce56843e45d0fd49ab05339db60253
Zain975 <zaingujjer97531@gmail.com>
2022-04-11 19:30:50 +0500

    api comit

--- routes/timezone.js
+++ routes/timezone.js
@@ -37,11 +37,11 @@ router.post("/timezone", async (req, res, err) => {
 
 // set Timezone
 
-// router.get("/time", function (req, res) {
-//   const nDate = new Date().toLocaleString("en-US", {
-//     timeZone: req.body.timeZone,
-//   });
-//   res.status(200).json(nDate);
-// });
+router.get("/time", function (req, res) {
+  const nDate = new Date().toLocaleString("en-US", {
+    timeZone: req.body.timeZone,
+  });
+  res.status(200).json(nDate);
+});
 
 module.exports = router;
