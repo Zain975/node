3b94102b881513e723d83a8ef29854498d6bdbc9
Zain975 <zaingujjer97531@gmail.com>
2022-04-12 08:11:34 +0500

    weather api updated

--- routes/weather.js
+++ routes/weather.js
@@ -4,6 +4,7 @@ const Weather = require("../models/Weather");
 const fetch = require("node-fetch");
 dotenv.config();
 
+// post lat and lon in DB
 router.post("/setLat&Lon", async (req, res, err) => {
   try {
     const weather = await Weather.create({
@@ -20,14 +21,33 @@ router.post("/setLat&Lon", async (req, res, err) => {
   }
 });
 
+// Update lat and lon
+
+router.put("/updateLat&Lon", async (req, res) => {
+  const weatherId = "62539b21cbc708a219b703f9";
+  const weather = await Weather.findByIdAndUpdate(weatherId);
+  if (weather) {
+    weather.lat = req.body.lat;
+    weather.lon = req.body.lon;
+    const updatedWeather = await weather.save();
+    res
+      .status(200)
+      .json({ status: true, message: "Successfully updated!", data: weather });
+  }
+});
+
+// Get weather from weather Api
+
 router.get("/lat&lon", async (req, res) => {
-  if (!req.query.lat) {
+  const weatherId = "62539b21cbc708a219b703f9";
+  const weather = await Weather.findById(weatherId);
+  if (!weather.lat) {
     res.status(404).json("lat is missing");
-  } else if (!req.query.lon) {
+  } else if (!weather.lon) {
     res.status(404).json("lon is missing");
   } else {
-    let lat = req.query.lat;
-    let lon = req.query.lon;
+    let lat = weather.lat;
+    let lon = weather.lon;
 
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`
@@ -36,6 +56,22 @@ router.get("/lat&lon", async (req, res) => {
     const data = await response.json();
     res.status(200).json({ status: true, message: "Succesfull", data: data });
   }
+
+  // if (!req.query.lat) {
+  //   res.status(404).json("lat is missing");
+  // } else if (!req.query.lon) {
+  //   res.status(404).json("lon is missing");
+  // } else {
+  //   let lat = req.query.lat;
+  //   let lon = req.query.lon;
+
+  //   const response = await fetch(
+  //     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`
+  //   );
+
+  //   const data = await response.json();
+  //   res.status(200).json({ status: true, message: "Succesfull", data: data });
+  // }
 });
 
 module.exports = router;
