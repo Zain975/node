c5731b475c92205c7fc0818b50cab435ce1601f0
Zain975 <zaingujjer97531@gmail.com>
2022-05-13 11:15:21 +0500

    background api

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-var mapp = require("./mapp");
-var http = require("http");
-var robot = require("robotjs");
-var os = require("os");
-var handleSocketEvents = require("./socket");
+// var mapp = require("./mapp");
+// var http = require("http");
+// var robot = require("robotjs");
+// var os = require("os");
+// var handleSocketEvents = require("./socket");
 
 const authRoute = require("./routes/auth");
 const audioRoute = require("./routes/audio");
 const videoRoute = require("./routes/video");
 const weatherRoute = require("./routes/weather");
 const timezoneRoute = require("./routes/timezone");
-const frontBgRoute = require("./routes/frontBg");
+const backgroundImgRoute = require("./routes/backgroundImg");
 
 dotenv.config();
 const port = process.env.PORT || 8000;
@@ -31,95 +31,95 @@ app.use((req, res, next) => {
   next();
 });
 
-/**
- * Get port from environment and store in Express.
- */
+// /**
+//  * Get port from environment and store in Express.
+//  */
 
-//  var port = normalizePort(process.env.PORT || '4000');
-mapp.set("port", port);
+// //  var port = normalizePort(process.env.PORT || '4000');
+// mapp.set("port", port);
 
-/**
- * Create HTTP server.
- */
+// /**
+//  * Create HTTP server.
+//  */
 
-var server = http.createServer(mapp);
+// var server = http.createServer(mapp);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
+// /**
+//  * Listen on provided port, on all network interfaces.
+//  */
 
-server.listen(port, "0.0.0.0");
-server.on("error", onError);
-server.on("listening", onListening);
+// server.listen(port, "0.0.0.0");
+// server.on("error", onError);
+// server.on("listening", onListening);
 
-var io = require("socket.io")(server);
+// var io = require("socket.io")(server);
 
-io.on("connection", (socket) => {
-  handleSocketEvents(socket, robot);
-  console.log("someon connected");
-});
+// io.on("connection", (socket) => {
+//   handleSocketEvents(socket, robot);
+//   console.log("someon connected");
+// });
 
-const ifaces = os.networkInterfaces();
-/**
- * Normalize a port into a number, string, or false.
- */
+// const ifaces = os.networkInterfaces();
+// /**
+//  * Normalize a port into a number, string, or false.
+//  */
 
-function normalizePort(val) {
-  var port = parseInt(val, 10);
+// function normalizePort(val) {
+//   var port = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
+//   if (isNaN(port)) {
+//     // named pipe
+//     return val;
+//   }
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+//   if (port >= 0) {
+//     // port number
+//     return port;
+//   }
 
-  return false;
-}
+//   return false;
+// }
 
-/**
- * Event listener for HTTP server "error" event.
- */
+// /**
+//  * Event listener for HTTP server "error" event.
+//  */
 
-function onError(error) {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
+// function onError(error) {
+//   if (error.syscall !== "listen") {
+//     throw error;
+//   }
 
-  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+//   var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
+//   // handle specific listen errors with friendly messages
+//   switch (error.code) {
+//     case "EACCES":
+//       console.error(bind + " requires elevated privileges");
+//       process.exit(1);
+//       break;
+//     case "EADDRINUSE":
+//       console.error(bind + " is already in use");
+//       process.exit(1);
+//       break;
+//     default:
+//       throw error;
+//   }
+// }
 
-/**
- * Event listener for HTTP server "listening" event.
- */
+// /**
+//  * Event listener for HTTP server "listening" event.
+//  */
 
-function onListening() {
-  Object.keys(ifaces).forEach((ifname) =>
-    ifaces[ifname].forEach((iface) => {
-      if (!iface.internal && iface.family === "IPv4")
-        console.log(
-          `Can access on your network with this http://${iface.address}:${port}`
-        );
-    })
-  );
-}
+// function onListening() {
+//   Object.keys(ifaces).forEach((ifname) =>
+//     ifaces[ifname].forEach((iface) => {
+//       if (!iface.internal && iface.family === "IPv4")
+//         console.log(
+//           `Can access on your network with this http://${iface.address}:${port}`
+//         );
+//     })
+//   );
+// }
 
 // app.get("/", (req, res) => res.status(200).send("hello world"));
 app.use("/api/auth", authRoute);
@@ -127,7 +127,7 @@ app.use("/api/uploads", audioRoute);
 app.use("/api/uploads", videoRoute);
 app.use("/api/weather", weatherRoute);
 app.use("/api/timezone", timezoneRoute);
-app.use("/api/setBackground", frontBgRoute);
+app.use("/api/setBackground", backgroundImgRoute);
 
 app.listen(port, () => {
   console.log("server is running");
