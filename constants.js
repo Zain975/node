6809f5aba713cf2b2dd7d99aca9ee58441c31ce9
Zain975 <zaingujjer97531@gmail.com>
2022-05-24 11:03:54 +0500

    uncoment the remote code

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
-// const constants = {
-//   MOUSE_MOVE: "mouse_move",
-//   MOUSE_CLICK: "mouse_click",
-//   KEY_TOGGLE: "key_toggle",
-// };
+const constants = {
+  MOUSE_MOVE: "mouse_move",
+  MOUSE_CLICK: "mouse_click",
+  KEY_TOGGLE: "key_toggle",
+};
 
-// module.exports = constants;
+module.exports = constants;
