f0c7bbf8532dacedb5e99d9e6163b203bd2fbcff
Zain975 <zaingujjer97531@gmail.com>
2022-06-01 16:27:38 +0500

    committ

--- constants.js
+++ constants.js
@@ -2,6 +2,7 @@ const constants = {
   MOUSE_MOVE: "mouse_move",
   MOUSE_CLICK: "mouse_click",
   KEY_TOGGLE: "key_toggle",
+  KEY_PRESS: "KEY_PRESS",
 };
 
 module.exports = constants;
--- socket.js
+++ socket.js
@@ -16,7 +16,37 @@ function handleSocketEvents(socket, robot) {
     console.log("click", button);
     robot.mouseClick(button, double);
   });
+  // keyboard
+  socket.on(constants.KEY_PRESS, (action) => {
+    let alt = action.alt;
+    let ctrl = action.ctrl;
+    let shift = action.shift;
+    let meta = action.meta;
+    let code = action.code;
+    let string = action.string;
 
+    if (alt) robot.keyToggle("alt", "down");
+    if (ctrl) robot.keyToggle("control", "down");
+    if (shift) robot.keyToggle("shift", "down");
+    if (meta) robot.keyToggle("command", "down");
+
+    // let specialKey = KEY_MAPPING[code]
+    if (string) {
+      robot.typeString(string);
+      // else if (specialKey) {
+      // robot.keyTap(specialKey)
+    } else {
+      // Hack for android where key code is always 229. We use the backspace
+      // when the string is empty.
+      robot.keyTap("backspace");
+    }
+
+    if (alt) robot.keyToggle("alt", "up");
+    if (ctrl) robot.keyToggle("control", "up");
+    if (shift) robot.keyToggle("shift", "up");
+    if (meta) robot.keyToggle("command", "up");
+    return false;
+  });
   // key toggle
   socket.on(constants.KEY_TOGGLE, ({ key, pressed }) => {
     const state = pressedToState[!!pressed];
