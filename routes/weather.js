9fabde72947c965200b14667fa6f6b36016a4158
Zain975 <zaingujjer97531@gmail.com>
2022-06-02 10:26:13 +0500

    commit

--- routes/weather.js
+++ routes/weather.js
@@ -50,7 +50,7 @@ router.get("/lat&lon", async (req, res) => {
     let lon = weather.lon;
 
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.API_KEY}`
     );
 
     const data = await response.json();
